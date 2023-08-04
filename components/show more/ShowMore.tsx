99c6f17b5e7a7c3a31f9e5c9b016320c08105ef6
Gauransh Juneja <gauranshjuneja@gmail.com>
2023-08-04 22:26:33 +0530

    final build

--- components/show more/ShowMore.tsx
+++ components/show more/ShowMore.tsx
@@ -5,13 +5,12 @@ import { ShowMoreProps } from '@/types'
 import CustomButton from '../Custom Button/CustomButton'
 import { updateSearchParams } from '@/app/utils'
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
   const router = useRouter()
 
   const handelNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
-    const newPathname = updateSearchParams("limit", newLimit.toString());
-    router.push(newPathname);
+    setLimit(newLimit);
   }
   return (
     <div className='w-full flex-center gap-5 mt-10'>
